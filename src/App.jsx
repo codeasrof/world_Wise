c6f4f5bd773716d02a0115243a90427e763e0639
codeasrof <vitalidade.08-devorador@icloud.com>
2023-11-13 17:36:01 -0300

    Page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Product from "./pages/Product"
+import HomePage from "./pages/HomePage"
+import Pricing from "./pages/Pricing"
+import PageNotFound from "./pages/PageNotFound"
+
 export default function App() {
   return (
-    <div>
-      <h1>World Wise</h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+        <Route path="   product" element={<Product/>}/>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="pricing" element={<Pricing/>} />
+        <Route path="*" element={<PageNotFound/>} />
+    </Routes>
+</BrowserRouter>
   )
 }
--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,17 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./HomePage";
-import Product from "./Product";
-
 export default function Pricing() {
   return (
     <>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/product" element={<Product/>}/>
-                <Route path="/" element={<HomePage/>} />
-                <Route path="/pricing" element={<Pricing/>} />
-            </Routes>
-        </BrowserRouter>
+        <h1>Pricing</h1>
     </>
   )
 }
