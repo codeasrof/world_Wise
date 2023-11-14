5147af6d9379b55b3297eb5424eea33651c609ec
codeasrof <vitalidade.08-devorador@icloud.com>
2023-11-14 00:04:09 -0300

    adding fake server

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Product from "./pages/Product"
-import HomePage from "./pages/HomePage"
 import Pricing from "./pages/Pricing"
 import PageNotFound from "./pages/PageNotFound"
 import AppLayout from "./pages/AppLayout"
 import Login from "./pages/Login"
+import CityList from "./components/CityList"
+import Homepage from "./pages/Homepage"
 
 export default function App() {
   return (
     <BrowserRouter>
     <Routes>
         <Route path="   product" element={<Product/>}/>
-        <Route path="/" element={<HomePage/>} />
+        <Route path="/" element={<Homepage/>} />
         <Route path="pricing" element={<Pricing/>} />
         <Route path="product" element={<Product/>} />
         <Route path="app" element={<AppLayout/>}>
 
-          <Route index element={<p>List</p>}/>
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route index element={<CityList/>}/>
+          <Route path="cities" element={<CityList/>} />
           <Route path="countries" element={<p>List of countries</p>} />
           <Route path="form" element={<p>List of form</p>} />
           
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,5 +1,9 @@
+import styles from "./CityList.module.css"
+
 export default function CityList() {
   return (
-    <div>CityList</div>
+    <ul className={styles.cityList}>
+      List
+    </ul>
   )
 }
