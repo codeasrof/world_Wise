b68012829e7dc778b67c88cbcf8f5d9479831ddc
codeasrof <vitalidade.08-devorador@icloud.com>
2023-11-14 00:44:38 -0300

    adding message if no cities has been selected

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,30 @@ import AppLayout from "./pages/AppLayout"
 import Login from "./pages/Login"
 import CityList from "./components/CityList"
 import Homepage from "./pages/Homepage"
+import { useEffect, useState } from "react"
+
+const BASE_URL = "http://localhost:8000"
 
 export default function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchCities = async () => {
+      setIsLoading(true)
+      try {
+        const res = await fetch(`${BASE_URL}/cities`)
+        const data = await res.json()
+        setCities(data)
+      } catch (error) {
+        console.error(error)
+      }finally{
+        setIsLoading(false)
+      }
+    }
+    fetchCities()
+  },[])
+
   return (
     <BrowserRouter>
     <Routes>
@@ -17,8 +39,8 @@ export default function App() {
         <Route path="product" element={<Product/>} />
         <Route path="app" element={<AppLayout/>}>
 
-          <Route index element={<CityList/>}/>
-          <Route path="cities" element={<CityList/>} />
+          <Route index element={<CityList cities={cities} isLoading={isLoading}/>}/>
+          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
           <Route path="countries" element={<p>List of countries</p>} />
           <Route path="form" element={<p>List of form</p>} />
           
