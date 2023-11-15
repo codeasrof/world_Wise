4d14009e4a6b340e20255e0e64cb062fc3d72d03
codeasrof <vitalidade.08-devorador@icloud.com>
2023-11-15 14:40:30 -0300

    Getting latitude and longitute based on url params

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,15 @@
+import { useSearchParams } from "react-router-dom"
 import styles from "./Map.module.css"
 
 export default function Map() {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const lat = searchParams.get("lat")
+  const lng = searchParams.get("lng")
+
   return (
-    <div className={styles.mapContainer}>Map</div>
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <p>Position: {lat} - {lng}</p>
+    </div>
   )
 }
